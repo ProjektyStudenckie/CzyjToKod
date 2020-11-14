25b7e859dee78c2b1debb8e4ec3fb8ca03dad80f
Jakub Sosna <jakub.sosna@icloud.com>
2020-11-14 13:27:01 +0100

    Create basic MVVM structure

---
+++ ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace CzyjToKod.ViewModel.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
