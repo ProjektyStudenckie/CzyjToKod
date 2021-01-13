a1e91365839712fa91fb54146e4cf25874cd5efe
wenkly-Wojciech-Wielochowski <wojciechwielochowski@wenklystudio.com>
2021-01-13 12:39:23 +0100

    add support for sliders and displaying results

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -7,20 +7,123 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CzyjToKod.ViewModel
 {
     class MainWindowViewModel : ViewModelBase
     {
+        private const string nameLevenshtein = "1. Levenshtein: ";
+        private const string nameDamerau = "2. Damerau Levenshtein: ";
+        private const string nameHamming = "3. Hamming: ";
+        private const string nameCosine= "1. Cosine: ";
+        private const string nameJaro = "2. Jaro: ";
+        private const string nameJaroWinkler = "3. Jaro Winkler: ";
 
         private string _file1Path = "";
         private string _file2Path = "";
 
+        private int _s1Value=1;
+        private int _s2Value=1;
+        private int _s3Value=1;
+
+        private string _Value1 = nameLevenshtein;
+        private string _Value2 = nameDamerau;
+        private string _Value3 = nameHamming;
+        private string _ValueDif1 = nameCosine;
+        private string _ValueDif2 = nameJaro;
+        private string _ValueDif3 = nameJaroWinkler;
+
         #region Public Properties
 
-        public string File1Path {
-            get => _file1Path; 
+        public int s1Value
+        {
+            get => _s1Value;
+            set => _s1Value = value;
+        }
+
+        public int s2Value
+        {
+            get => _s2Value;
+            set => _s2Value = value;
+        }
+
+        public int s3Value
+        {
+            get => _s3Value;
+            set => _s3Value = value;
+        }
+
+        public string Value1
+        {
+            get => _Value1;
+            set
+            {
+                _Value1 = nameLevenshtein + value;
+                onPropertyChanged(nameof(Value1));
+
+            }
+        }
+
+        public string Value2
+        {
+            get => _Value2;
+            set
+            {
+                _Value2 = nameDamerau + value;
+                onPropertyChanged(nameof(Value2));
+
+            }
+        }
+
+        public string Value3
+        {
+            get => _Value3;
+            set
+            {
+                _Value3 = nameHamming + value;
+                onPropertyChanged(nameof(Value3));
+
+            }
+        }
+
+        public string ValueDif1
+        {
+            get => _ValueDif1;
+            set
+            {
+                _ValueDif1 = nameCosine + value;
+                onPropertyChanged(nameof(ValueDif1));
+
+            }
+        }
+
+        public string ValueDif2
+        {
+            get => _ValueDif2;
+            set
+            {
+                _ValueDif2 = nameJaro + value;
+                onPropertyChanged(nameof(ValueDif2));
+
+            }
+        }
+
+        public string ValueDif3
+        {
+            get => _ValueDif3;
+            set
+            {
+                _ValueDif3 = nameJaroWinkler + value;
+                onPropertyChanged(nameof(ValueDif3));
+
+            }
+        }
+
+        public string File1Path
+        {
+            get => _file1Path;
             set
             {
                 _file1Path = value;
@@ -29,7 +132,8 @@ namespace CzyjToKod.ViewModel
                 {
                     File1Length = $"Characters: {File.ReadAllText(value).Length}";
                     File1Invalid = "";
-                } catch(Exception e)
+                }
+                catch (Exception e)
                 {
                     File1Invalid = e.Message;
                     File1Length = "";
@@ -88,12 +192,13 @@ namespace CzyjToKod.ViewModel
                 if (_handleGetFileClick == null)
                 {
                     _handleGetFileClick = new RelayCommand(
-                        arg => {
+                        arg =>
+                        {
 
                             OpenFileDialog openFileDialog = new OpenFileDialog();
                             if (openFileDialog.ShowDialog() == true)
                             {
-                                if((string)arg == "1")
+                                if ((string)arg == "1")
                                 {
                                     File1Path = openFileDialog.FileName;
                                 }
@@ -101,7 +206,7 @@ namespace CzyjToKod.ViewModel
                                 {
                                     File2Path = openFileDialog.FileName;
                                 }
-                                   
+
                                 onPropertyChanged(nameof(File1Path));
                                 onPropertyChanged(nameof(File2Path));
                             }
@@ -125,7 +230,8 @@ namespace CzyjToKod.ViewModel
                 if (_handleCheckClick == null)
                 {
                     _handleCheckClick = new RelayCommand(
-                        arg => {
+                        arg =>
+                        {
 
                             if (File1Invalid.Length == 0 && File2Invalid.Length == 0)
                             {
@@ -147,7 +253,6 @@ namespace CzyjToKod.ViewModel
                 return _handleCheckClick;
             }
         }
-
         #endregion
 
         private async void WaitForResult()
@@ -159,15 +264,31 @@ namespace CzyjToKod.ViewModel
                 var jsonString = File.ReadAllText(Reinterpreter.Direction + "\\results.txt");
                 var results = JsonSerializer.Deserialize<ResultsData>(jsonString);
 
-                // tu możesz otrzymać dostęp do danych z jsona tak jak w tym Logu
-                Console.WriteLine(results.cosine.ToString()+ results.damerau_levenshtein_distance.ToString()+ results.hamming_distance.ToString()+ results.levenshtein_distance.ToString()+ results.jaro_similarity.ToString()+ results.jaro_winkler_similarity.ToString());
-                float value = results.cosine;
-                float result = value * 100;
+                SetUiResults(results);
+
+                float wholeWeight = s1Value + s2Value + s3Value;
+
+                float valueCosine = results.cosine * s1Value;
+                float valueJaro = results.jaro_similarity * s2Value;
+                float valueJaroWinkler = results.jaro_winkler_similarity * s3Value;
+                float result = ((valueCosine + valueJaro + valueJaroWinkler) / wholeWeight) * 100;
                 int resultInt = (int)result;
                 Result = resultInt.ToString();
                 onPropertyChanged(nameof(Result));
             });
- 
+
         }
+
+        private void SetUiResults(ResultsData results)
+        {
+
+            Value1 = results.levenshtein_distance.ToString();
+            Value2 = results.damerau_levenshtein_distance.ToString();
+            Value3 = results.hamming_distance.ToString();
+            ValueDif1 = results.cosine.ToString();
+            ValueDif2 = results.jaro_similarity.ToString();
+            ValueDif3 = results.jaro_winkler_similarity.ToString();
+        }
+
     }
 }
